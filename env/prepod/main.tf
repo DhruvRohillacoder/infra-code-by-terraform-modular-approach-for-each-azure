dc4f97044495934ff3ae8ce72be6542aac92360c
Dhruv <dhruvrohilla9350@gmail.com>
2025-10-27 21:30:44 +0530

    add rg comment

--- env/prepod/main.tf
+++ env/prepod/main.tf
@@ -1,8 +1,8 @@
 module "Rg" {
   source          = "../../modules/azurerm_resource_group"
-  resource_groups = var.resource_groups
+  resource_groups = var.resource_groups //rg
 }
 module "stg" {
   source           = "../../modules/azurerm_storage_account"
-  storage_accounts = var.storage_accounts
+  storage_accounts = var.storage_accounts //stg
 }
--- modules/azurerm_storage_account/terraform.tfvars
+++
@@ -1,8 +0,0 @@
-storage_accounts = {
-  "stg1" = {
-    name                     = "stg1"
-    location                 = "eastus"
-    account_tier             = "Standard"
-    account_replication_type = "LRS"
-  }
-}
